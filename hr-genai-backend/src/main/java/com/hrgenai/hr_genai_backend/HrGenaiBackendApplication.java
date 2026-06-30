b0623459a3e9b8397603ce1f7cc0bf5624da2ccd
Ayush-js <ayushmishraamlai34@gmail.com>
2026-06-30 22:52:49 +0530

    Uploading backend files

---
+++ hr-genai-backend/src/main/java/com/hrgenai/hr_genai_backend/HrGenaiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hrgenai.hr_genai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrGenaiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrGenaiBackendApplication.class, args);
+	}
+
+}
---
+++ hr-genai-backend/src/test/java/com/hrgenai/hr_genai_backend/HrGenaiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrgenai.hr_genai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrGenaiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
